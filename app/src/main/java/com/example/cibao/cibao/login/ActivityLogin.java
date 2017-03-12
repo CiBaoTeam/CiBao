0a1cefe88229842283e2fd7f8278a0252e5ce236
BenQuickDeNN <2413269761@qq.com>
2017-03-12 15:30:43 +0800

    initial project

---
+++ app/src/main/java/com/example/cibao/cibao/login/ActivityLogin.java
@@ -0,0 +1,20 @@
+package com.example.cibao.cibao.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cibao.cibao.R;
+
+/**
+ * 屈彬
+ * 2017/3/12
+ * 登录活动
+ */
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
