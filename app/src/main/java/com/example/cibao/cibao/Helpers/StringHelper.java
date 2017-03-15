3aa6db89e47fb50447a8d6ccbe36d79aae483c95
BenQuickDeNN <2413269761@qq.com>
2017-03-16 00:10:46 +0800

    add helper class

--- app/src/main/java/com/example/cibao/cibao/Helpers/StringHelper.java
+++ app/src/main/java/com/example/cibao/cibao/Helpers/StringHelper.java
@@ -6,4 +6,13 @@ package com.example.cibao.cibao.Helpers;
  * 字符串处理类
  */
 public class StringHelper {
+    /**
+     * @show 判断两组字符串是否相同
+     * @param str1 比较字符串1
+     * @param str2 比较字符串2
+     * @return 返回字符串比较结果，相同为true。否则为false
+     */
+    public static boolean isEqual(String str1, String str2){
+        return str1.equals(str2);
+    }
 }
