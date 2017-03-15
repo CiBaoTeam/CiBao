9504569d84e54526b6d0434e209872451eb58fcb
BenQuickDeNN <2413269761@qq.com>
2017-03-16 00:00:23 +0800

    fix login render problems

--- app/src/main/java/com/example/cibao/cibao/login/main_level/ActivityLogin.java
+++ app/src/main/java/com/example/cibao/cibao/login/main_level/ActivityLogin.java
@@ -1,15 +1,10 @@
-package com.example.cibao.cibao.login;
+package com.example.cibao.cibao.login.main_level;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.cibao.cibao.R;
 
-/**
- * 屈彬
- * 2017/3/12
- * 登录活动
- */
 public class ActivityLogin extends AppCompatActivity {
 
     @Override
