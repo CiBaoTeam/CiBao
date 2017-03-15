33eeec853212e9fc0d5d95b977003b3715bdf771
BenQuickDeNN <2413269761@qq.com>
2017-03-16 00:05:33 +0800

    add helper class

---
+++ app/src/main/java/com/example/cibao/cibao/Helpers/RemoteInterfaceHelper.java
@@ -0,0 +1,9 @@
+package com.example.cibao.cibao.Helpers;
+
+/**
+ * 屈彬
+ * 2017/3/16
+ * 远程接口辅助类
+ */
+public class RemoteInterfaceHelper {
+}
--- app/src/main/java/com/example/cibao/cibao/login/main_level/ActivityLogin.java
+++ app/src/main/java/com/example/cibao/cibao/login/main_level/ActivityLogin.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.cibao.cibao.R;
-
+/**
+ * 屈彬
+ * 2017/3/16
+ * 登录活动
+ */
 public class ActivityLogin extends AppCompatActivity {
 
     @Override
